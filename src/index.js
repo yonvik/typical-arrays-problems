da246f75da57e79ac229b6c18df0bf514b5cf9c6
Andrey Yankovskiy <tiamoonrock@gmail.com>
2020-09-29 13:47:02 +0300

    feat: index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0
+  }
+  return Math.min.apply(null, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0
+  }
+  return Math.max.apply (null,array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+   
+  if (!array || array.length === 0) {
+    return 0
+  }
+  const sum = array.reduce((a,b)  => a + b, 0 );
+  return sum / array.length;
 }
